0d139b5b02c08090d89bea76dbe89f9d4225c900
pujansharma <sharmapujan209@gmail.com>
2023-10-16 09:42:38 +0530

    added

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # chatbot
+
+
+"id": "chatcmpl-89CJTzITY1XIWv6G6fMQFg1Vd9KP5",
+    "object": "chat.completion",
+    "created": 1697202975,
+    "model": "gpt-3.5-turbo-0613",
+
+
+    "id": "chatcmpl-89CKLklHeyhJUFvBeNA5cDmeBTgPF",
+    "object": "chat.completion",
+    "created": 1697203029,
+    "model": "gpt-3.5-turbo-0613",
--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,24 @@
 const express = require('express');
+const {userdata}=require("./model/user.model");
+const jwt=require("jsonwebtoken");
+const {Auth}=require("./middleware/Auth")
+const bcrypt=require("bcrypt");
 const axios = require('axios');
-const cors=require("cors");
+const cors = require("cors");
+const {connection}=require("./connection")
+const {chatmodel}=require("./model/chatmodel")
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 const app = express();
 app.use(express.json());
 app.use(cors())
-require('dotenv').config(); 
+require('dotenv').config();
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
-app.post('/ask', async (req, res) => {
+app.post('/ask', Auth, async (req, res) => {
     try {
-        const {user_input} = req.body;
-        
-        let response = await fetch(`https://api.openai.com/v1/chat/completions`, {
+        const { user_input } = req.body;
+        const user = req.userId; // Assuming that req.userId contains the user's ID after using the Auth middleware
+
+        const response = await fetch(`https://api.openai.com/v1/chat/completions`, {
             method: "POST",
             headers: {
                 "Authorization": `Bearer ${OPENAI_API_KEY}`,
@@ -19,28 +26,130 @@ app.post('/ask', async (req, res) => {
             },
             body: JSON.stringify({
                 model: "gpt-3.5-turbo",
-                messages: [{ role: "system", content: "You are a helpful assistant." },
-                { role: "user", content: user_input },],
-                
+                messages: [
+                    { role: "system", content: "Always provide an answer about Earth only if a relevant question arises; otherwise, simply respond with 'I don't know." },
+                    { role: "user", content: user_input },
+                ],
             })
         });
-  
-        response = await response.json();
-  
-        // Check if response.choices is defined and not empty
-        if (response.choices && response.choices.length > 0) {
-            const data = response.choices[0].message.content;
+
+        const responseJson = await response.json();
+
+        if (responseJson.choices && responseJson.choices.length > 0) {
+            const data = responseJson.choices[0].message.content;
+
+            const chatData = {
+                conversationId: responseJson.id,
+                user: user, // Associate the chat message with the currently logged-in user
+                message: [
+                    {
+                        user_input: user_input,
+                        role: responseJson.choices[0].message.role,
+                        content: responseJson.choices[0].message.content,
+                        timestamp: new Date()
+                    }
+                ]
+            };
+
+            // Save the chat data
+            const chat = new chatmodel(chatData);
+            await chat.save();
+
             res.status(200).send({ code: data });
         } else {
-            // Handle the case when response.choices is empty
             res.status(500).send({ msg: "No valid response from the API" });
         }
     } catch (error) {
         console.log(error);
         res.status(500).send({ msg: error.message });
     }
-  })
-  let port=3000
-app.listen(3000, () => {
-  console.log(`Server is running on port ${port}`);
+});
+
+
+
+app.get('/getChat/:id', Auth,async (req, res) => {
+    const chatId = req.params.id;
+    try {
+        const data = await chatmodel.findOne({ _id: chatId }); // Use _id
+        if (data) {
+            res.status(200).send(data);
+        } else {
+            res.status(404).send({ msg: 'Chat not found' });
+        }
+    } catch (error) {
+        res.status(500).send({ msg: 'Error retrieving chat data' });
+    }
+});
+app.get("/getchat", Auth, async (req, res) => {
+    try {
+        const user = await userdata.findOne({ _id: req.userId }); // Use req.userId to find the user
+
+        if (!user) {
+            return res.status(404).send({ msg: 'User not found' });
+        }
+
+        const data = await chatmodel.find({ user: user._id });
+        res.status(200).send(data);
+    } catch (error) {
+        res.status(500).send({ msg: 'Server error' });
+    }
+});
+
+
+
+app.post("/register", async (req, res) => {
+    try {
+        const { firstname, lastname, email, password } = req.body;
+
+        // Hash the user's password before saving it to the database
+        const hashpass = await bcrypt.hash(password, 10);
+
+        const user = new userdata({
+            firstname,
+            lastname,
+            email,
+            password: hashpass, // Store the hashed password in the database
+        });
+
+        await user.save();
+        res.status(200).send("Register successful");
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);
+    }
+});
+
+
+//login user
+app.post("/login", async(req,res)=>{
+    try {
+        const {email,password}=req.body;
+        const user=await userdata.findOne({email});
+        if(!user){
+            res.status(401).send("Invaild credentails");
+            return;
+        }
+        const passvaild=await bcrypt.compare(password,user.password);
+         if(!passvaild){
+            res.status(401).send("Invaild credentails");
+            return;
+         }
+         const token=jwt.sign({userId:user._id},"masai");
+         res.json({token})
+    } catch (error) {
+        res.status(500).send("error login");
+    }
+})
+
+
+
+let port = 3000
+app.listen(3000, async() => {
+    try {
+        await connection;
+        console.log("connected to database")
+    } catch (error) {
+        console.log("something wrong in database")
+    }
+    console.log(`Server is running on port ${port}`);
 });
